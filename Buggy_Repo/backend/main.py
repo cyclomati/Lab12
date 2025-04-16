afbe0bbb4e929f638f27e9701f005521bfc78f55
cyclomati <156425271+cyclomati@users.noreply.github.com>
2025-04-16 17:01:53 +0530

    Update main.py

--- Buggy_Repo/backend/main.py
+++ Buggy_Repo/backend/main.py
@@ -3,8 +3,24 @@ from routes.items import router as items_router
 from routes.analytics import router as analytics_router
 from routes.quiz import router as quiz_router
 
+from routes.quiz import router as quiz_router
+
+from fastapi.middleware.cors import CORSMiddleware  
+
+
+
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 👈 Or restrict to ["http://localhost:5500"] if serving from a specific port
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+app.include_router(quiz_router)
 app.include_router(items_router, prefix="/items")
 app.include_router(analytics_router)
 app.include_router(quiz_router)
